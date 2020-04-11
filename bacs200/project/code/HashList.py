be0267977963a42ed49cf9f875c684c2d19e831c
kort8325 <60017897+kort8325@users.noreply.github.com>
2020-04-11 12:40:23 -0600

    Create HashList.py

---
+++ bacs200/project/code/HashList.py
@@ -0,0 +1,54 @@
+class HashList:
+    def __init__(self, length):
+        self.hlist = [None] * length
+        self.len = length
+
+    def print(self):
+        print(self.hlist)
+
+    def hashfunction(self, item):
+        return item % self.len
+
+    def rehashfunction(self, oldpos):
+        return (oldpos + 1) % self.len
+
+    def contains(self, item):
+        for i in self.hlist:
+            if(i==item):
+                return True
+        return False
+                    
+    def put(self, item):
+        hashvalue = self.hashfunction(item)
+        if self.hlist[hashvalue] == None:
+            self.hlist[hashvalue] =item
+        elif self.hlist[hashvalue]!=None:
+            return self.put2(hashvalue+1,item,hashvalue,0)
+
+    def put2(self, hashvalue,item,origin,counter):
+        if self.hlist[hashvalue]==None:
+            self.hlist[hashvalue]=item
+        elif self.hlist[hashvalue]!=None:
+            if hashvalue==origin:
+                return print("error")
+            if hashvalue == self.len-1:
+                return self.put2(0,item, origin, counter)
+            else:
+                return self.put2(hashvalue+1,item,origin,counter)
+
+    def items(self):
+        string = ""
+        for i in self.hlist:
+            if i != None:
+                string += str(i) + ", "
+        return string
+
+
+""" # test Hash List 
+hashlist = HashList(5)
+hashlist.put(6)
+hashlist.put(8)
+hashlist.put(14)
+print(hashlist.contains(6))
+hashlist.print()
+print(hashlist.items()) """
